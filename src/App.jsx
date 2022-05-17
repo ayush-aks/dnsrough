06e670db19a2d29d5140fd2f1885e828cdcd199b
ayush-aks <88273791+ayush-aks@users.noreply.github.com>
2022-05-17 10:58:18 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,253 @@
+import React from "react";
+
+function App() {
+  return <Homepage {...homepageData} />;
+}
+
+export default App;
+
+function Homepage(props) {
+  const {
+    antRozetskyHxolltswrpmUnsplash1,
+    oneStopToFulfillYourLocalNeeds,
+    raiseRequestOrVie,
+    aBridgeBetweenSer,
+    logo_Nobrand1,
+    title,
+    text1,
+    raiseRequestForLocalNeed,
+    ifYouFeelTheNeed,
+    web_Development_11,
+    helpingBusinessOpportunists,
+    weAlsoHelpBusines,
+    web_Development_12,
+    viewLocalNeedsPos,
+    mobile_Phone1,
+    youCanAlsoSeeWha,
+    raiseARequestNow,
+    overlapGroup,
+    overlapGroup1,
+    dS1,
+    letYourLocalNeedsBeHeard,
+    signUpToGetStarted,
+    line4,
+    privacyPolicy,
+    termsOfService,
+    place,
+    text3,
+    text4,
+    text2,
+    x2022DSAllRightsReserved,
+    moreAboutUs,
+    rocket_Launch_1,
+    demandServiceAll,
+    dS2,
+    logo_Nobrand2,
+    getStarted,
+    login,
+  } = props;
+
+  return (
+    <div className="container-center-horizontal">
+      <div className="homepagescreen">
+        <div className="overlap-group6">
+          <div className="page-container"></div>
+          <img className="ant-rozetsky-hx-oll-t-swrp-m-unsplash-1" src={antRozetskyHxolltswrpmUnsplash1} />
+          <div className="rectangle-2"></div>
+          <div className="one-stop-to-fulfill-your-local-needsroboto-medium-white-43px">
+            {oneStopToFulfillYourLocalNeeds}
+          </div>
+          <div className="raise-request-or-vie">
+            {raiseRequestOrVie}
+          </div>
+          <div className="a-bridge-between-serroboto-medium-white-43px">
+            {aBridgeBetweenSer}
+          </div>
+          <img className="logo_nobrand-1" src={logo_Nobrand1} alt="logo"/>
+          <h1 className="title">
+            {title}
+          </h1>
+          <div className="ellipse-1"></div>
+          <div className="text-1">
+            {text1}
+          </div>
+          <div className="service-1animate-enter">
+            <div className="raise-request-for-local-needroboto-medium-shark-40px">
+              {raiseRequestForLocalNeed}
+            </div>
+            <div className="if-you-feel-the-needroboto-normal-boulder-30px">
+              {ifYouFeelTheNeed}
+            </div>
+            <img className="web_development_-1" src={web_Development_11} alt="web_dev"/>
+          </div>
+          <div className="service-3">
+            <div className="helping-business-opportunistsroboto-medium-shark-40px">
+              {helpingBusinessOpportunists}
+            </div>
+            <div className="we-also-help-businesroboto-normal-boulder-30px">
+              {weAlsoHelpBusines}
+            </div>
+            <img className="web_development_-1-1" src={web_Development_12} alt="web_dev"/>
+          </div>
+          <div className="service-2">
+            <div className="view-local-needs-posroboto-medium-shark-40px">
+              {viewLocalNeedsPos}
+            </div>
+            <img className="mobile_phone-1" src={mobile_Phone1} alt="mob_phn"/>
+            <div className="you-can-also-see-wharoboto-normal-boulder-30px">
+              {youCanAlsoSeeWha}
+            </div>
+          </div>
+          <div className="rectangle-8"></div>
+          <div className="raise-a-request-now">
+            {raiseARequestNow}
+          </div>
+          <div className="footer">
+            <div className="overlap-group" style={{ backgroundImage: `url(${overlapGroup})` }}>
+              <div className="overlap-group1" style={{ backgroundImage: `url(${overlapGroup1})` }}>
+                <div className="ds">
+                  {dS1}
+                </div>
+              </div>
+              <div className="let-your-local-needs-be-heard">
+                {letYourLocalNeedsBeHeard}
+              </div>
+              <div className="overlap-group2">
+                <div className="sign-up-to-get-started">
+                  {signUpToGetStarted}
+                </div>
+              </div>
+              <img className="line-4" src={line4} alt="line4"/>
+              <div className="flex-row">
+                <div className="privacy-policyroboto-medium-white-13px">
+                  {privacyPolicy}
+                </div>
+                <div className="terms-of-serviceroboto-medium-white-13px">
+                  {termsOfService}
+                </div>
+                <div className="placeroboto-medium-white-13px">
+                  {place}
+                </div>
+                <div className="text-3fontawesome-regular-normal-white-22px">
+                  {text3}
+                </div>
+                <div className="text-4fontawesome-regular-normal-white-22px">
+                  {text4}
+                </div>
+              </div>
+            </div>
+            <div className="text-2">
+              {text2}
+            </div>
+            <p className="x2022-ds-all-rights-reserved">
+              {x2022DSAllRightsReserved}
+            </p>
+          </div>
+          <div className="overlap-group3">
+            <div className="rectangle-10"></div>
+            <div className="more-about-us">
+              {moreAboutUs}
+            </div>
+            <img className="rocket_launch_-1" src={rocket_Launch_1} alt="rocket"/>
+            <div className="demand-service-all">
+              {demandServiceAll}
+            </div>
+          </div>
+          <div className="navbar">
+            <div className="overlap-group5">
+              <div className="ds-1">
+                {dS2}
+              </div>
+              <img className="logo_nobrand-2" src={logo_Nobrand2} alt="logo"/>
+            </div>
+            <div className="overlap-group4">
+              <div className="get-started">
+                {getStarted}
+              </div>
+            </div>
+            <div className="login">
+              {login}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const homepageData = {
+  antRozetskyHxolltswrpmUnsplash1: "ant-rozetsky-hxolltswrpm-unsplash-1.png",
+  oneStopToFulfillYourLocalNeeds: "One stop to fulfill your local needs.",
+  raiseRequestOrVie: "Raise request or view local needs in your area - All Online!",
+  aBridgeBetweenSer: "A Bridge between service seekers and Business Opportunists!",
+  logo_Nobrand1: "logo-nobrand-1.png",
+  title: "D&S",
+  text1: "",
+  raiseRequestForLocalNeed: "Raise request for local Need!",
+  ifYouFeelTheNeed: (
+    <React.Fragment>
+      If you feel the need for a local service which is Not <br />
+      available in your area, you can raise a request for
+      <br />
+      the same right here.
+    </React.Fragment>
+  ),
+  web_Development_11: "web-development-1.png",
+  helpingBusinessOpportunists: "Helping Business Opportunists",
+  weAlsoHelpBusines: (
+    <React.Fragment>
+      We also help Business Opportunists who are looking
+      <br />
+      for investment opportunities by providing a map:-
+      <br />
+      This map will help to better visualize what <br />
+      service do people of certain areas feel the need of.
+    </React.Fragment>
+  ),
+  web_Development_12: "web-development-1-1.png",
+  viewLocalNeedsPos: (
+    <React.Fragment>
+      View local needs posted by other users <br />
+      in your area.
+    </React.Fragment>
+  ),
+  mobile_Phone1: "mobile-phone-1.png",
+  youCanAlsoSeeWha: (
+    <React.Fragment>
+      You can also see what service request other users <br />
+      near your location have posted. You can: <br />
+      Upvote ifyou feel that need is valid OR <br />
+      Downvote if you feel the need is a hoax.
+    </React.Fragment>
+  ),
+  raiseARequestNow: "Raise a request now",
+  overlapGroup: "rectangle-7.png",
+  overlapGroup1: "logo-nobrand-2.png",
+  dS1: "D&S",
+  letYourLocalNeedsBeHeard: "Let your Local needs be Heard!",
+  signUpToGetStarted: "Sign Up to Get Started",
+  line4: "line-4.png",
+  privacyPolicy: "Privacy Policy",
+  termsOfService: "Terms of Service",
+  place: "Contact",
+  text3: "",
+  text4: "",
+  text2: "",
+  x2022DSAllRightsReserved: "2022 D&S All rights reserved",
+  moreAboutUs: "More about Us:",
+  rocket_Launch_1: "rocket-launch-1.png",
+  demandServiceAll: (
+    <React.Fragment>
+      Demand &amp; Service allows users to raise their demands for local service on this platform.
+      <br />
+      <br />
+      We provide this information to businesss opportunists to setup their business in demanded regions to grow exponentially. <br />
+      <br />
+      Our website benefits both consumers and businessmen.
+    </React.Fragment>
+  ),
+  dS2: "D&S",
+  logo_Nobrand2: "logo-nobrand-2-1.png",
+  getStarted: "Get Started",
+  login: "Login",
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react";
+import App from "./App";
+
+ReactDOM.render(<div><App /></div>,document.getElementById("root"));
